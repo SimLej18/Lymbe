1f8b76725394a89364b14f37226956dfd585dcf1
SimLej18 <simonlejoly@icloud.com>
2024-01-12 14:30:38 +0100

    Deploy

--- README.md
+++
@@ -1,3 +0,0 @@
-# Lymbe
-
-A personal library of Rive and CSS animations and effects, mainly for the purpose of having all my creations and exercises in the same place and see my personnal progress at a glance.
--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import { base } from '$app/paths';
-</script>
-
-<div class="w-screen h-screen text-center flex flex-col justify-center">
-  <a href="{base}/" class="text-3xl pb-10">Welcome to the LYMBE</a>
-  <p>My own personal place to keep everything fancy I make in Rive and CSS.</p>
-</div>
-
