605ea8278610151df6229fe643e111f56a2ff21f
SimLej18 <simonlejoly@icloud.com>
2024-01-12 14:17:13 +0100

    [feat] ready for gh-pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+</script>
+
 <div class="w-screen h-screen text-center flex flex-col justify-center">
-  <h1 class="text-3xl pb-10">Welcome to the LYMBE</h1>
+  <a href="{base}/" class="text-3xl pb-10">Welcome to the LYMBE</a>
   <p>My own personal place to keep everything fancy I make in Rive and CSS.</p>
 </div>
 
