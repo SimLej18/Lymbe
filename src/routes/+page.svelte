b1758789874c9c8efe3f6c4d2bbfc4294516fbfc
SimLej18 <simonlejoly@icloud.com>
2024-01-12 14:09:05 +0100

    [feat] tailwind + daisyui + basic welcome page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-screen h-screen text-center flex flex-col justify-center">
+  <h1 class="text-3xl pb-10">Welcome to the LYMBE</h1>
+  <p>My own personal place to keep everything fancy I make in Rive and CSS.</p>
+</div>
+
